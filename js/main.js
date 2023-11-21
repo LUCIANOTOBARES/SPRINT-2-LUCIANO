8b60d3a9cba0316a3ab2a634aa63062ff894ce07
luciano <lucianoezequieltobares@gmail.com>
2023-11-21 20:44:42 -0300

    final3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-let boton =  document.getElementById("btn");
-boton.addEventListener("click",function(){
-     
-let titulo = document.getElementById("registro"); 
-titulo.innerHTML = "<h2>Se enviaron tus datos correctamente!!!</h2>";
-titulo.classList.add("verde")
-titulo.classList.remove("gracias")
-titulo.style.color= "red"
-titulo.style.backgroundColor
+let boton = document.getElementById("btn");
+boton.addEventListener("click", function () {
+
+    let titulo = document.getElementById("registro");
+    titulo.innerHTML = "<h2>Se enviaron tus datos correctamente!!!</h2>";
+    titulo.classList.add("verde")
+    titulo.classList.remove("gracias")
+    titulo.style.color = "red"
+
+
+    const menuButton = document.querySelector('.boton-h');
+    const menuList = document.querySelector('menu');
+
+    menuButton.addEventListener('click', () => {
+        menuList.classList.toggle('menu-active');
+    });
+
 
 })
 
-
-
-
-
-    
 
---
+++ readme.txt
@@ -0,0 +1,19 @@
+Mi pagina se trata de venta de pasteleria y reposteria.
+
+contenido de mi pagina wed:
+-titulo
+-subtitulo
+-barra de navegación(inicio,productos,sobre nosotros)
+-carousel (imagenes de tortas)
+-breve información de la panaderia
+-ubicación (maps)
+-datos(forma de pago, envios,contactos)
+-formulario(datos personales del usuario)
+-alerta(confirmación de los datos)
+
+-framework utilizado: bootstrap
+
+-lenguaje utilizado:
+-html
+-css
+-js
