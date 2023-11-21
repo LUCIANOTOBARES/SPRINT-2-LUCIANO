070ea6330509b99f17d75ed5248819105b93d2fd
luciano <lucianoezequieltobares@gmail.com>
2023-11-21 20:12:22 -0300

    final

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let boton =  document.getElementById("btn");
+boton.addEventListener("click",function(){
+     
+let titulo = document.getElementById("registro"); 
+titulo.innerHTML = "<h2>Se enviaron tus datos correctamente!!!</h2>";
+titulo.classList.add("verde")
+titulo.classList.remove("gracias")
+titulo.style.color= "red"
+titulo.style.backgroundColor
+
+})
+
+
+
+
+
+    
+
